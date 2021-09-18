3a466dbb11dba1551e41792a3e84a629cb0009de
lilman109 <akira.yoshidasjsu@gmail.com>
2021-09-18 17:44:05 +0900

    added leaflet map api and added map to project using geolocation

--- script.js
+++ script.js
@@ -10,3 +10,29 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    position => {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
+
+      const coords = [latitude, longitude];
+      const map = L.map('map').setView(coords, 15);
+
+      L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+        maxZoom: 20,
+        subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+    },
+    () => {
+      alert('Could not get yoru position');
+    }
+  );
+}
